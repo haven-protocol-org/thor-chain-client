b7175301399cc24915857c9fa8b8f2992948c19b
akildemir <34187742+akildemir@users.noreply.github.com>
2020-12-07 18:33:32 +0300

    get block test added

--- haven-test-harness.go
+++ haven-test-harness.go
@@ -6,117 +6,172 @@ import (
 	"io"
 	"net/rpc"
 
+	// "encoding/json"
+
 	"github.com/powerman/rpc-codec/jsonrpc2"
 )
 
-type BlockHeader struct {
-}                     
-
 type GetInfo_Result struct {
-     Alt_Blocks_Count int
-     Bloc_Size_Limit uint
-     Block_Size_Median uint
-     Block_Weight_Limit uint
-     Block_Weight_Median uint
-     Bootstrap_Daemon_Address string
-     Cumulative_Difficulty int
-     Cumulative_Difficulty_Top64 int
-     Database_Size int
-     Difficulty int64
-     Difficulty_Top64 int64
-     Free_Space int64
-     Grey_Peerlist_Size int
-     Height int
-     Height_Without_Bootstrap int
-     Incoming_Connections_Count int
-     Mainnet bool
-     Nettype string
-     Offline bool
-     Outgoing_Connections_Count int
-     Rpc_Connections_Count int
-     Stagenet bool
-     Start_Time int
-     Status string
-     Target int
-     Target_Height int
-     Testnet bool
-     Top_Block_Hash string
-     Tx_Count int
-     Tx_Pool_Size int
-     Untrusted bool
-     Update_Available bool
-     Version string
-     Was_Bootstrap_Ever_Used bool
-     White_Peerlist_Size int
-     Wide_Cumulative_Difficulty string
-     Wide_Difficulty string
+	Alt_Blocks_Count            int
+	Bloc_Size_Limit             uint
+	Block_Size_Median           uint
+	Block_Weight_Limit          uint
+	Block_Weight_Median         uint
+	Bootstrap_Daemon_Address    string
+	Cumulative_Difficulty       int
+	Cumulative_Difficulty_Top64 int
+	Database_Size               int
+	Difficulty                  int64
+	Difficulty_Top64            int64
+	Free_Space                  int64
+	Grey_Peerlist_Size          int
+	Height                      int
+	Height_Without_Bootstrap    int
+	Incoming_Connections_Count  int
+	Mainnet                     bool
+	Nettype                     string
+	Offline                     bool
+	Outgoing_Connections_Count  int
+	Rpc_Connections_Count       int
+	Stagenet                    bool
+	Start_Time                  int
+	Status                      string
+	Target                      int
+	Target_Height               int
+	Testnet                     bool
+	Top_Block_Hash              string
+	Tx_Count                    int
+	Tx_Pool_Size                int
+	Untrusted                   bool
+	Update_Available            bool
+	Version                     string
+	Was_Bootstrap_Ever_Used     bool
+	White_Peerlist_Size         int
+	Wide_Cumulative_Difficulty  string
+	Wide_Difficulty             string
 }
 
 type GetVersion_Result struct {
-     Status string
-     Untrusted bool
-     Version int
+	Status    string
+	Untrusted bool
+	Version   int
+}
+
+type Block_Header struct {
+	Block_Size    int
+	Depth         int
+	Difficulty    int64
+	Hash          string
+	Height        int64
+	Major_version int
+	Minor_version int
+	Nonce         int64
+	Num_txes      int
+	Orphan_status bool
+	Prev_hash     string
+	Reward        int64
+	Timestamp     int64
+}
+
+type BLOCK struct {
+	Blob          string
+	Block_Header  Block_Header
+	Json          string
+	Miner_Tx_Hash string
+	Status        string
+	Untrusted     bool
+	Tx_Hashes     []string
+}
+
+func GetBlock() {
+
+	// Connect to daemon RPC server
+	clientHTTP := jsonrpc2.NewHTTPClient("http://127.0.0.1:27750/json_rpc")
+	defer clientHTTP.Close()
+
+	type REQ struct {
+		height int
+	}
+
+	var myreq REQ
+	myreq.height = 5005
+
+	var reply BLOCK
+	var err error
+
+	// Get Height
+	err = clientHTTP.Call("get_block", &myreq, &reply)
+	if err == rpc.ErrShutdown || err == io.ErrUnexpectedEOF {
+		fmt.Printf("Error(): %q\n", err)
+	} else if err != nil {
+		rpcerr := jsonrpc2.ServerError(err)
+		fmt.Printf("Error(): code=%d msg=%q data=%v reply=%v\n", rpcerr.Code, rpcerr.Message, rpcerr.Data, reply)
+	}
+
+	fmt.Printf("RESULT: %d ", reply.Block_Header.Nonce)
 }
 
 func GetHeight() (error, int) {
-     
-     // Connect to daemon RPC server
-     clientHTTP := jsonrpc2.NewHTTPClient("http://127.0.0.1:17750/json_rpc")
-     defer clientHTTP.Close()
 
-     var reply GetInfo_Result
-     var err error
+	// Connect to daemon RPC server
+	clientHTTP := jsonrpc2.NewHTTPClient("http://127.0.0.1:17750/json_rpc")
+	defer clientHTTP.Close()
 
-     // Get Height
-     err = clientHTTP.Call("get_info", nil, &reply)
-     if err == rpc.ErrShutdown || err == io.ErrUnexpectedEOF {
-     	 fmt.Printf("Error(): %q\n", err)
-     } else if err != nil {
-         rpcerr := jsonrpc2.ServerError(err)
-	 fmt.Printf("Error(): code=%d msg=%q data=%v reply=%v\n", rpcerr.Code, rpcerr.Message, rpcerr.Data, reply)
-     }
+	var reply GetInfo_Result
+	var err error
 
-     return err, reply.Height
+	// Get Height
+	err = clientHTTP.Call("get_info", nil, &reply)
+	if err == rpc.ErrShutdown || err == io.ErrUnexpectedEOF {
+		fmt.Printf("Error(): %q\n", err)
+	} else if err != nil {
+		rpcerr := jsonrpc2.ServerError(err)
+		fmt.Printf("Error(): code=%d msg=%q data=%v reply=%v\n", rpcerr.Code, rpcerr.Message, rpcerr.Data, reply)
+	}
+
+	return err, reply.Height
 }
 
 func GetVersion() (error, string) {
-     
-     // Connect to daemon RPC server
-     clientHTTP := jsonrpc2.NewHTTPClient("http://127.0.0.1:17750/json_rpc")
-     defer clientHTTP.Close()
 
-     var reply GetInfo_Result
-     var err error
+	// Connect to daemon RPC server
+	clientHTTP := jsonrpc2.NewHTTPClient("http://127.0.0.1:17750/json_rpc")
+	defer clientHTTP.Close()
 
-     // Get Height
-     err = clientHTTP.Call("get_info", nil, &reply)
-     if err == rpc.ErrShutdown || err == io.ErrUnexpectedEOF {
-     	 fmt.Printf("Err3(): %q\n", err)
-     } else if err != nil {
-         rpcerr := jsonrpc2.ServerError(err)
-	 fmt.Printf("Err3(): code=%d msg=%q data=%v reply=%v\n", rpcerr.Code, rpcerr.Message, rpcerr.Data, reply)
-     }
+	var reply GetInfo_Result
+	var err error
 
-     return err, reply.Version
+	// Get Height
+	err = clientHTTP.Call("get_info", nil, &reply)
+	if err == rpc.ErrShutdown || err == io.ErrUnexpectedEOF {
+		fmt.Printf("Err3(): %q\n", err)
+	} else if err != nil {
+		rpcerr := jsonrpc2.ServerError(err)
+		fmt.Printf("Err3(): code=%d msg=%q data=%v reply=%v\n", rpcerr.Code, rpcerr.Message, rpcerr.Data, reply)
+	}
+
+	return err, reply.Version
 }
 
 func main() {
 
-     // Local vars
-     var status error
-     var height int
-     var version string
+	// Local vars
+	// var status error
+	// var height int
+	// var version string
 
-     // Get the height of the chain
-     status, height = GetHeight()
-     if (status != nil) {
-     } else {
-	fmt.Printf("Height = %d\n", height)
-     }
-     
-     status, version = GetVersion()
-     if (status != nil) {
-     } else {
-	fmt.Printf("Version = %s\n", version)
-     }
+	// Get the height of the chain
+	// status, height = GetHeight()
+	// if (status != nil) {
+	// } else {
+	// fmt.Printf("Height = %d\n", height)
+	// }
+
+	// status, version = GetVersion()
+	// if (status != nil) {
+	// } else {
+	// fmt.Printf("Version = %s\n", version)
+	// }
+
+	GetBlock()
 }
