47f6d39780a98101f349521973d49d534dfecc54
akildemir <34187742+akildemir@users.noreply.github.com>
2020-12-16 19:11:06 +0300

    private key functions finalised

--- key_sign_wrapper.go
+++ key_sign_wrapper.go
@@ -1,10 +1,9 @@
 package haven
 
 import (
-	"errors"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec"
+	moneroCrypto "github.com/haven-protocol-org/monero-go-utils/crypto"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/tendermint/tendermint/crypto"
@@ -17,7 +16,8 @@ import (
 // KeySignWrapper is a wrap of private key and also tss instance
 // it also implement the txscript.Signable interface, and will decide which method to use based on the pubkey
 type KeySignWrapper struct {
-	privateKey      *btcec.PrivateKey
+	privViewKey     [32]byte
+	privSpendKey    [32]byte
 	pubKey          common.PubKey
 	tssKeyManager   tss.ThorchainKeyManager
 	logger          zerolog.Logger
@@ -25,13 +25,14 @@ type KeySignWrapper struct {
 }
 
 // NewKeysignWrapper create a new instance of Keysign Wrapper
-func NewKeySignWrapper(privateKey *btcec.PrivateKey, bridge *thorclient.ThorchainBridge, tssKeyManager tss.ThorchainKeyManager, keySignPartyMgr *thorclient.KeySignPartyMgr) (*KeySignWrapper, error) {
-	pubKey, err := GetBech32AccountPubKey(privateKey)
+func NewKeySignWrapper(privViewKey *[32]byte, privSpendKey *[32]byte, bridge *thorclient.ThorchainBridge, tssKeyManager tss.ThorchainKeyManager, keySignPartyMgr *thorclient.KeySignPartyMgr) (*KeySignWrapper, error) {
+	pubKey, err := GetBech32AccountPubKey(privSpendKey)
 	if err != nil {
 		return nil, fmt.Errorf("fail to get the pubkey: %w", err)
 	}
 	return &KeySignWrapper{
-		privateKey:      privateKey,
+		privViewKey:     privViewKey,
+		privSpendKey:    privSpendKey,
 		pubKey:          pubKey,
 		tssKeyManager:   tssKeyManager,
 		logger:          log.Logger.With().Str("module", "keysign_wrapper").Logger(),
@@ -40,20 +41,24 @@ func NewKeySignWrapper(privateKey *btcec.PrivateKey, bridge *thorclient.Thorchai
 }
 
 // GetBech32AccountPubKey calculate the pubkey given private key
-func GetBech32AccountPubKey(key *btcec.PrivateKey) (common.PubKey, error) {
-	// TODO: change name of this function from GetBech32AccountPubKey to getPubKeyFromPrivateKey
-	buf := key.PubKey().SerializeCompressed()
+func GetBech32AccountPubKey(key *[32]byte) (common.PubKey, error) {
+	var buf [32]byte
+	moneroCrypto.PublicFromSecret(&buf, key)
 	var pk secp256k1.PubKeySecp256k1
 	copy(pk[:], buf)
 	return common.NewPubKeyFromCrypto(pk)
 }
 
 // getHavenPrivateKey contructs a private key from a thorchain private key
-func getHavenPrivateKey(key crypto.PrivKey) (*btcec.PrivateKey, error) {
+func getHavenPrivateKey(key crypto.PrivKey) (secretViewKey *[32]byte, secretSpendKey *[32]byte) {
 	priKey, ok := key.(secp256k1.PrivKeySecp256k1)
-	if !ok {
-		return nil, errors.New("invalid private key type")
-	}
-	privateKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), priKey[:])
-	return privateKey, nil
+	// generate secret spend key
+	h := moneroCrypto.NewHash()
+	var keyHash [32]byte
+	h.Write(priKey[:])
+	h.Sum(keyHash[:0])
+	moneroCrypto.SecretFromSeed(secretSpendKey, &keyHash)
+	// genere secret view key
+	moneroCrypto.ViewFromSpend(secretViewKey, secretSpendKey)
+	return
 }
